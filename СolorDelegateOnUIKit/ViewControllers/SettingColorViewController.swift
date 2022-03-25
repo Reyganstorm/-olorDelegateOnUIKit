2f902fd48bf78ad5cd4907ad7e8be86a368b6c94
Ruslan <ruslan.shtybaev@gmail.com>
2022-03-25 20:31:03 +0600

    put the files in order

---
+++ СolorDelegateOnUIKit/ViewControllers/SettingColorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingColorViewController.swift
+//  СolorDelegateOnUIKit
+//
+//  Created by Руслан Штыбаев on 25.03.2022.
+//
+
+import UIKit
+
+class SettingColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
